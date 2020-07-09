d0fa873f4ed8cb5a383deb13c7c1306ec80057fa
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-07-09 15:49:43 +0200

    Add version 8.1.0-dev.0

---
+++ v8.1.0-dev.0/reason-react/ReasonReactCompat.bs.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ReasonReact = require("./ReasonReact.bs.js");
+
+var wrapReactForReasonReact = ReasonReact.wrapJsForReason;
+
+var wrapReasonReactForReact = ReasonReact.wrapReasonForJs;
+
+exports.wrapReactForReasonReact = wrapReactForReasonReact;
+exports.wrapReasonReactForReact = wrapReasonReactForReact;
+/* ReasonReact Not a pure module */
