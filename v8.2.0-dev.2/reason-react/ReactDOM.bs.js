440ec3f3cf07b444f4468ad0cc0c28e724158526
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-07-28 10:52:52 +0200

    Add v8.2.0-dev.2

---
+++ v8.2.0-dev.2/reason-react/ReactDOM.bs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var Experimental = {};
+
+var Ref = {};
+
+var Props = {};
+
+var Style;
+
+exports.Experimental = Experimental;
+exports.Ref = Ref;
+exports.Props = Props;
+exports.Style = Style;
+/* No side effect */
