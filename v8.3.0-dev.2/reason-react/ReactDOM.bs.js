3dcf9287965a8c06b19e4a26633ac233e9d0cd1a
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-09-15 20:43:34 +0200

    Add v8.3.0-dev.2 release

---
+++ v8.3.0-dev.2/reason-react/ReactDOM.bs.js
@@ -0,0 +1,19 @@
+
+
+
+var Experimental = {};
+
+var Ref = {};
+
+var Props = {};
+
+var Style;
+
+export {
+  Experimental ,
+  Ref ,
+  Props ,
+  Style ,
+  
+}
+/* No side effect */
---
+++ v8.3.0-dev.2/reason-react/ReasonReactCompat.bs.js
@@ -0,0 +1,14 @@
+
+
+import * as ReasonReact from "./ReasonReact.bs.js";
+
+var wrapReactForReasonReact = ReasonReact.wrapJsForReason;
+
+var wrapReasonReactForReact = ReasonReact.wrapReasonForJs;
+
+export {
+  wrapReactForReasonReact ,
+  wrapReasonReactForReact ,
+  
+}
+/* ReasonReact Not a pure module */
